c7e6fbbdde13f39a79f9a7ebac8a4abb789b5a31
Elijah <elijahs2106@gmail.com>
2019-11-06 19:33:15 -0600

    Numbers work for keypress and clicks on button

--- calc.js
+++ calc.js
@@ -1,3 +1,59 @@
+let displayValue = "0";
+
+const calculator = document.getElementById("calculator");
+const numberInput = document.getElementById("numberInput");
+const inputs = calculator.children;
+
+numberInput.addEventListener('input', e => updateDisplayValue(e.target.value));
+
+document.addEventListener("keypress", e => {
+  console.log(event.keyCode);
+  if (event.keyCode > 47 && event.keyCode < 58) {
+    numPress(event.keyCode - 48);
+  }
+})
+
+document.addEventListener('keydown', e => {
+  if (event.keyCode == 8) {
+    eraseNum();
+  }
+})
+
+for (let i = 0; i < inputs.length; i++) {
+  const inputElement = inputs[i];
+
+  switch(inputElement.innerHTML) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+      inputElement.addEventListener('click', (e) => {
+        numPress(e.target.innerText);
+      });
+      break;
+    case 'x':
+    case '+':
+    case '-':
+    case '/':
+    case '%':
+      break;
+    case '+/-':
+      break;
+    case '=':
+      break;
+    case 'AC':
+      break;
+    case '.':
+      break;
+  }
+}
+
 const add = function(a, b) {
   return a + b;
 }
@@ -25,4 +81,17 @@ const operate = function(operator, a, b) {
     case '/':
       return divide(a, b); 
   }
+}
+
+const numPress = function(num) {
+  updateDisplayValue(String((Number(displayValue) * 10) + Number(num)));
+}
+
+const eraseNum = function() {
+  updateDisplayValue(displayValue.slice(0,-1));
+}
+
+const updateDisplayValue = function(newDisplay) {
+  displayValue = newDisplay === "" ? "0" : newDisplay;
+  numberInput.value = displayValue;
 }
